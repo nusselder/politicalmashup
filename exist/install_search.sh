02a67e2322f48867fab823508f4aa5ff68eb0fe4
Arjan Nusselder <github@nusselder.eu>
2014-08-01 16:21:21 +0200

    Updated/created three install scripts that detail three common scenarios.

--- exist/install_search.sh
+++ exist/install_search.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Perform all installation steps to arrive at a running ode.politicalmashup.nl.
+# Perform all installation steps to arrive at a running search.politicalmashup.nl.
 #
 # Note, it is more clear to do every step individually.
 # When using this script, supply as argument the password you are going to
@@ -9,8 +9,8 @@ SCRIPT_ABS_PATH="$(cd "${0%/*}" 2>/dev/null; echo "$PWD")"
 
 PASSWORD="$@"
 
-$SCRIPT_ABS_PATH/prepare_ode.sh
+$SCRIPT_ABS_PATH/prepare_search.sh
 $SCRIPT_ABS_PATH/initial_install.sh
-time $SCRIPT_ABS_PATH/setup_apps.sh "ode"
+time $SCRIPT_ABS_PATH/setup_apps.sh "politicalmashup"
 time $SCRIPT_ABS_PATH/setup_data_xml.sh "$PASSWORD"
 
