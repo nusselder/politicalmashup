0870b2b74c1c66faf16ba6af5ff8668c53313acd
Arjan Nusselder <github@nusselder.eu>
2014-08-04 13:24:42 +0200

    password supplied check added to pm install scripts

--- exist/install_search.sh
+++ exist/install_search.sh
@@ -9,6 +9,12 @@ SCRIPT_ABS_PATH="$(cd "${0%/*}" 2>/dev/null; echo "$PWD")"
 
 PASSWORD="$@"
 
+if [ -z "$PASSWORD" ]; then
+  echo "Please supply as first (and only) argument the exact password"
+  echo "that will be used during the installation step."
+  exit 1
+fi
+
 $SCRIPT_ABS_PATH/prepare_search.sh
 $SCRIPT_ABS_PATH/initial_install.sh
 time $SCRIPT_ABS_PATH/setup_apps.sh "politicalmashup"
